093e85b5e7566d5a141e14d9fcfe41da9eb73459
PiotrAdamczyk <piotr.adamczyk@whirly.pl>
2022-11-10 17:52:19 +0100

    init

---
+++ src/main/java/com/example/dddcomponents/DddComponentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddcomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddComponentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddComponentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddcomponents/DddComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddcomponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddComponentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
