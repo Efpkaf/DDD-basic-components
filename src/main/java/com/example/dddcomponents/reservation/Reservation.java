d80e921bb994fcd3c31b472c670ed07b28ee64a2
PiotrAdamczyk <piotr.adamczyk@whirly.pl>
2022-11-10 18:01:15 +0100

    added aggregate annotation

--- src/main/java/com/example/dddcomponents/reservation/Reservation.java
+++ src/main/java/com/example/dddcomponents/reservation/Reservation.java
@@ -1,12 +1,16 @@
 package com.example.dddcomponents.reservation;
 
+import com.example.dddcomponents.sharedKernel.Aggregate;
 import com.example.dddcomponents.sharedKernel.AggregateRoot;
 
+@Aggregate
 public class Reservation extends AggregateRoot<String> {
 
     public Reservation(String id) {
         super(id);
     }
+
+
 
 
 }
